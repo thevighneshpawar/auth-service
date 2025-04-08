7e33c435ca49f7a3f46b564a734a986006f88555
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-04-08 19:50:27 +0530

    Project setup template

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { Config } from './config'
+import app from './app'
+import logger from './config/logger'
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            logger.info('Listening on port ', { port: PORT })
+        })
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+startServer()
